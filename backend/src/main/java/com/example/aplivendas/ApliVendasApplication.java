64e21137794651d4ef5f859c2be8d06650f4bcbb
Reinaldo <reinaldorib28@gmail.com>
2021-09-09 20:34:15 -0300

    projeto criado

---
+++ backend/src/main/java/com/example/aplivendas/ApliVendasApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApliVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApliVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/aplivendas/ApliVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplivendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApliVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
