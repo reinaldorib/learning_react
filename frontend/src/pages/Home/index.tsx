3532a0a0eb773d35843c6a7016db37d5b5dd96e8
Reinaldo <reinaldorib28@gmail.com>
2021-09-12 14:12:52 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,25 @@
+import Footer from "components/footer"
+import NavBar from "components/NavBar"
 
 const Home = () => {
     return (
-      <div>
-        <h1>home</h1>      
-      </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <link className="btn btn-primary btn-lg"  to="Dashboard">
+                        Acessar Dashboard
+                    </link>
+                </div>
+            </div>
+            <Footer />
+        </>
+        
     );
-  }
-  
-  export default Home;
+}
+
+export default Home;
